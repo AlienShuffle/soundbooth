79c30910b7d64a4d88162095383b28a9c01a0015
AlienShuffle <100056147+AlienShuffle@users.noreply.github.com>
2026-02-21 17:31:09 -0500

    Enhance comments in config-crestron.sh
    Added comments to clarify the purpose of commands and configuration details.

--- web-scripts/config-crestron.sh
+++ web-scripts/config-crestron.sh
@@ -5,6 +5,9 @@
 # SETAVUROUTE Make a Audio-Video-USB connection
 # DUMPDMROUTEINFO dump all current routing info
 # DUMPDMROUTEINFO n - dump routing for slot n
+# MESS is an echo command
+# SHOWHW is a detail hardware dump report
+# BYE is logout gracefully
 # REPORTDM Report port info (summarized here)
 # input slots range 1-8
 # output slots range 17-24
@@ -34,4 +37,9 @@
     sleep 1
     echo -ne "bye\r"
 ) |
-    pv -qL 8 | nc -q 1 dmmd8x8.cbclocal 23 
+    pv -qL 8 | nc -q 1 dmmd8x8.cbclocal 23
+    # pv is used to slow down input to deal with slow telnet response on Crestron.
+    # nc is netcat and sends the stream of echo's to the device on port 23.
+    # -q 1 says to quit 1 second after the stream ends to help with a graceful shutdown of connection.
+    # Note the Crestron DNS entry is hard-coded here and can be changed.
+    # It was originally configure in the Unifi Network Manager for the CBC lan.
